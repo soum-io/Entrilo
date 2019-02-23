1f9dc4c75827a4e40d3faf83f68c8d97cfea6843
Kanika Punhani <kpunhan2@illinois.edu>
2019-02-23 03:05:23 -0600

    organized frontend

---
+++ client/src/Components/Home/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+
+
+  render() {
+    return (
+        <div>
+         <button className="Login">
+          Login
+          </button>
+
+          <button className="Sign Up">
+          Sign Up
+          </button>
+       </div>
+
+    );
+  }
+}
+export default App;
